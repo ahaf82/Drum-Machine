a552677f08cf9ce7f186b5bea52f4559e924f564
Armin Hafner <a_h82@gmx.net>
2020-02-09 13:53:55 +0100

    Updates

---
+++ precache-manifest.9d27cec52f25f37220fba3a0ca009993.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1732411d39cd5c5be3b5826e90cde888",
+    "url": "/drum-machine/index.html"
+  },
+  {
+    "revision": "43fff26566852464e70a",
+    "url": "/drum-machine/static/css/main.a5fc958b.chunk.css"
+  },
+  {
+    "revision": "25fcc36b048c7557ce34",
+    "url": "/drum-machine/static/js/2.7a1b34fc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/drum-machine/static/js/2.7a1b34fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43fff26566852464e70a",
+    "url": "/drum-machine/static/js/main.2bb2dd25.chunk.js"
+  },
+  {
+    "revision": "507a9231a935b1c59061",
+    "url": "/drum-machine/static/js/runtime-main.e050a463.js"
+  }
+]);
